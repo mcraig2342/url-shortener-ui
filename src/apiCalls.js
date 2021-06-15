d232dadc99fffddb395531b1c0b0bd0908812246
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-15 09:55:51 -0600

    Write post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,15 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (url) => {
+  fetch("http://localhost:3001/api/v1/urls", {
+    method: "POST",
+    body: {long_url: url.url, title: url.title},
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+  .catch(err => console.log('ERROR', err))
+
+}
